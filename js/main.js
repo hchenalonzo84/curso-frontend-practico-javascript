8496f802b21d9c50d4e6e1dae02e0a18ccb059d1
unknown <roland8410@hotmail.com>
2023-01-05 17:47:07 -0600

    version 1.3 jueves_04_enero

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const menuemail= document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuemail.addEventListener('click', function() {
     console.log('recibiendo click');
   desktopMenu.classList.toggle('inactive');
+});
+
+menuBurger.addEventListener('click', function () {
+  mobileMenu.classList.toggle('inactive');
 });
 
 console.log('js funcionando');
